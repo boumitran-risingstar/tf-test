cfdf9bd57ef15e6c370bcbb505bcd1e15fd584e4
boumitran <boumitran@32studio.org>
2025-10-23 07:20:12 +0000

    Changes before Firebase Studio auto-run

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,10 @@ terraform {
       source  = "hashicorp/google"
       version = ">= 5.38.0"
     }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = ">= 5.38.0"
+    }
   }
 }
 
@@ -12,16 +16,28 @@ provider "google" {
   project = "tf-test-476002"
 }
 
+provider "google-beta" {
+  project = "tf-test-476002"
+}
+
+data "google_project" "project" {}
+
 resource "google_project_service" "run" {
-  service = "run.googleapis.com"
+  service                    = "run.googleapis.com"
+  disable_dependent_services = true
+  disable_on_destroy         = true
 }
 
 resource "google_project_service" "artifactregistry" {
-  service = "artifactregistry.googleapis.com"
+  service                    = "artifactregistry.googleapis.com"
+  disable_dependent_services = true
+  disable_on_destroy         = true
 }
 
 resource "google_project_service" "compute" {
-  service = "compute.googleapis.com"
+  service                    = "compute.googleapis.com"
+  disable_dependent_services = true
+  disable_on_destroy         = true
 }
 
 resource "google_artifact_registry_repository" "repository" {
@@ -45,69 +61,52 @@ resource "google_cloud_run_v2_service" "default" {
     type    = "TRAFFIC_TARGET_ALLOCATION_TYPE_LATEST"
   }
 
-  ingress = "INGRESS_TRAFFIC_INTERNAL_LOAD_BALANCER"
-}
-
-resource "google_compute_network" "default" {
-  name                    = "vps-network"
-  auto_create_subnetworks = false
-}
-
-resource "google_compute_subnetwork" "default" {
-  name          = "vps-subnetwork"
-  ip_cidr_range = "10.0.0.0/24"
-  region        = "us-central1"
-  network       = google_compute_network.default.id
-}
-
-resource "google_compute_subnetwork" "proxy_only" {
-  name          = "proxy-only-subnet"
-  purpose       = "REGIONAL_MANAGED_PROXY"
-  role          = "ACTIVE"
-  ip_cidr_range = "10.0.1.0/24"
-  region        = "us-central1"
-  network       = google_compute_network.default.id
+  ingress = "INGRESS_TRAFFIC_INTERNAL_ONLY"
 }
 
 resource "google_compute_region_network_endpoint_group" "serverless_neg" {
   name                  = "hello-world-neg"
-  region                = "us-central1"
   network_endpoint_type = "SERVERLESS"
+  region                = "us-central1"
   cloud_run {
     service = google_cloud_run_v2_service.default.name
   }
 }
 
-resource "google_compute_region_backend_service" "default" {
-  name                            = "backend-service"
-  region                          = "us-central1"
-  protocol                        = "HTTP"
-  timeout_sec                     = 30
-  load_balancing_scheme           = "INTERNAL_MANAGED"
+resource "google_compute_backend_service" "backend_service" {
+  name      = "hello-world-backend-service"
+  protocol  = "HTTP"
+  port_name = "http"
+  timeout_sec = 30
+  enable_cdn = true
+
+  log_config {
+    enable = true
+    sample_rate = 1.0
+  }
+
   backend {
     group = google_compute_region_network_endpoint_group.serverless_neg.id
   }
 }
 
-resource "google_compute_region_url_map" "default" {
-  name            = "url-map"
-  region          = "us-central1"
-  default_service = google_compute_region_backend_service.default.id
+resource "google_compute_url_map" "url_map" {
+  name            = "hello-world-url-map"
+  default_service = google_compute_backend_service.backend_service.id
 }
 
-resource "google_compute_region_target_http_proxy" "default" {
-  name    = "http-proxy"
-  region  = "us-central1"
-  url_map = google_compute_region_url_map.default.id
+resource "google_compute_target_http_proxy" "http_proxy" {
+  name    = "hello-world-http-proxy"
+  url_map = google_compute_url_map.url_map.id
 }
 
-resource "google_compute_forwarding_rule" "default" {
-  name                  = "forwarding-rule"
-  region                = "us-central1"
-  ip_protocol           = "TCP"
-  port_range            = "80"
-  load_balancing_scheme = "INTERNAL_MANAGED"
-  target                = google_compute_region_target_http_proxy.default.id
-  network               = google_compute_network.default.id
-  subnetwork            = google_compute_subnetwork.default.id
+resource "google_compute_global_forwarding_rule" "forwarding_rule" {
+  name       = "hello-world-forwarding-rule"
+  target     = google_compute_target_http_proxy.http_proxy.id
+  port_range = "80"
+}
+
+output "load_balancer_ip" {
+  description = "The IP address of the Global Load Balancer."
+  value       = google_compute_global_forwarding_rule.forwarding_rule.ip_address
 }
