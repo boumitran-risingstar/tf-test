129b167043887186cae914ce772948d9425256ab
boumitran <boumitran@32studio.org>
2025-10-23 03:48:19 +0000

    Changes before Firebase Studio auto-run

--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  res.send('Hello, from the root path!');
-});
-
-app.get('/hello', (req, res) => {
   res.send('Hello, World!');
 });
 
