cee3daac261dbad40078cd8d1e1b9969a2b2eed6
boumitran <boumitran@32studio.org>
2025-10-24 04:12:03 +0000

    code refactor

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,11 +16,6 @@ terraform {
   }
 }
 
-variable "project_id" {
-  description = "The Google Cloud project ID."
-  type        = string
-}
-
 provider "google" {
   project = var.project_id
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,9 @@
 
+variable "project_id" {
+  description = "The Google Cloud project ID."
+  type        = string
+}
+
 variable "use_load_balancer" {
   description = "If true, a global HTTPS load balancer, WAF, and CDN will be configured. If false, Cloud Run's native domain mapping will be used instead."
   type        = bool
