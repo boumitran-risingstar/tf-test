7424752988578dc40c3bcbd89d733a177bd810e0
boumitran <boumitran@32studio.org>
2025-10-26 12:25:40 +0000

    fixes

--- auth-ui/src/firebase/config.ts
+++ auth-ui/src/firebase/config.ts
@@ -3,12 +3,12 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBWEwPAbYKc_92cmtyRvTqAmC4boRtR3dc",
-  authDomain: "tf-test-476002.firebaseapp.com",
-  projectId: "tf-test-476002",
-  storageBucket: "tf-test-476002.appspot.com",
-  messagingSenderId: "731717657303",
-  appId: "1:731717657303:web:594b0fd90f1d0cf9b30692"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
