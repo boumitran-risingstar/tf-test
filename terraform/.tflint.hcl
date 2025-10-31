92f476b4e7297efc72cd524aee59e79273706de7
boumitran <boumitran@32studio.org>
2025-10-31 08:45:26 +0000

    remove terraform

--- terraform/.tflint.hcl
+++
@@ -1,5 +0,0 @@
-plugin "google" {
-  enabled = true
-  version = "0.25.0"
-  source  = "github.com/terraform-linters/tflint-ruleset-google"
-}
--- terraform/firestore-iam.tf
+++
@@ -1,5 +0,0 @@
-resource "google_project_iam_member" "firestore_user" {
-  project = var.project_id
-  role    = "roles/datastore.user"
-  member  = "serviceAccount:${google_service_account.users_api.email}"
-}
